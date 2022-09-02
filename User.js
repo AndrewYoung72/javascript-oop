b1f2300de2ba4195cbcd6e2521aaa7923aa51de5
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-02 15:53:38 -0600

    push before break

--- User.js
+++ User.js
@@ -10,16 +10,31 @@ export default class User {
   logout() {
     console.log(this.name, "just dropped one and walked away");
   }
-  displayUser() {
-    const displayUser = document.querySelector("#container");
-    displayUser.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const user = displayUser.querySelector("input", "text").value;
-      console.log(user);
-    })
-  }
 }
 let Bob = new User("Bob@bob.com", "Bob", "heresbob");
+
+//Display user
+
+const displayUser = document.querySelector(".container");
+displayUser.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = displayUser.querySelector("#email", "text").value;
+  const name = displayUser.querySelector("#name", "text").value;
+  console.log(email, name);
+});
+
+//create element
+const div = document.createElement("div")
+const userEmail = document.createElement("span");
+const userName = document.createElement("span");
+
+userEmail.textContent = email;
+userName.textContent = name;
+
+div.appendChild(userEmail);
+div.appendChild(userName);
+displayUser.appendChild(div)
+
 
 // the "new" keyword
 // creates a bew empty object {}
